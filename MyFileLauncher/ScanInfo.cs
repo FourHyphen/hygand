cc2d57ae87085fa6783ac012240635ab9e9184f1
FourHyphen <FourHyphen.4@gmail.com>
2024-04-14 17:29:40 +0900

    検索ディレクトリ設定ファイルから検索するしないディレクトリ一覧取得する処理を実装

---
+++ MyFileLauncher/ScanInfo.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+
+namespace MyFileLauncher
+{
+    internal class ScanInfo
+    {
+        private static readonly string ScanInfoFileName = "Scan.info";
+
+        public IReadOnlyCollection<string> ScanDirectories { get; }
+
+        public IReadOnlyCollection<string> NotScanDirectories { get; }
+
+        public static ScanInfo CreateInstance()
+        {
+            string scanInfoFilePath = System.IO.Path.GetFullPath(ScanInfoFileName);
+            return CreateInstance(scanInfoFilePath);
+        }
+
+        public static ScanInfo CreateInstance(string scanInfoFilePath)
+        {
+            return new ScanInfo(scanInfoFilePath);
+        }
+
+        private ScanInfo(string scanInfoFilePath)
+        {
+            string[] contents = System.IO.File.ReadAllLines(scanInfoFilePath);
+            ScanDirectories = GetScanDirectories(contents);
+            NotScanDirectories = GetNotScanDirectories(contents);
+        }
+
+        /// <summary>
+        /// 検索ディレクトリの一覧を返す
+        /// </summary>
+        private HashSet<string> GetScanDirectories(string[] contents)
+        {
+            // 先頭が "-" ではないディレクトリパスを検索ディレクトリとする
+            // 空行は対象外
+            return contents.Where(s => s != "" && s[0] != '-')
+                           .ToHashSet();
+        }
+
+        /// <summary>
+        /// 検索しないディレクトリの一覧を返す
+        /// </summary>
+        private HashSet<string> GetNotScanDirectories(string[] contents)
+        {
+            // 先頭が "-" のディレクトリパスを検索しないディレクトリととする
+            // 空行は対象外
+            return contents.Where(s => s != "" && s[0] == '-')
+                           .Select(s => s.Substring(1))    // 先頭の "-" を除外
+                           .ToHashSet();
+        }
+    }
+}
---
+++ TestMyFileLauncher/UnitTestScanInfo.cs
@@ -0,0 +1,57 @@
+﻿namespace TestMyFileLauncher
+{
+    [TestClass]
+    public class UnitTestScanInfo
+    {
+        private string BeforeEnvironment { get; set; } = Environment.CurrentDirectory;
+
+        [TestInitialize]
+        public void Init()
+        {
+            BeforeEnvironment = Environment.CurrentDirectory;
+            Environment.CurrentDirectory = Common.GetEnvironmentDirPath();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Environment.CurrentDirectory = BeforeEnvironment;
+        }
+
+        /// <summary>
+        /// 検索ディレクトリ一覧を設定ファイルから取得する
+        /// </summary>
+        [TestMethod]
+        public void GetDirectoriesToScanFromScanInfoFile()
+        {
+            MyFileLauncher.ScanInfo si = CreateScanInfo();
+
+            // 先頭が "-" でないディレクトリは検索する
+            Assert.IsTrue(si.ScanDirectories.Contains(@"C:\with space"));
+            Assert.IsTrue(si.ScanDirectories.Contains(@"D:\日本語\"));
+
+            Assert.AreEqual(expected: 2, actual: si.ScanDirectories.Count);
+        }
+
+        /// <summary>
+        /// 検索しないディレクトリ一覧を設定ファイルから取得する
+        /// </summary>
+        [TestMethod]
+        public void GetDirectoriesNotScanFromScanInfoFile()
+        {
+            MyFileLauncher.ScanInfo si = CreateScanInfo();
+
+            // 先頭が "-" のディレクトリは検索しない(先頭の "-" が消されていることも確認)
+            Assert.IsTrue(si.NotScanDirectories.Contains(@"C:\Program Files"));
+            Assert.IsTrue(si.NotScanDirectories.Contains(@"E:\"));
+
+            Assert.AreEqual(expected: 2, actual: si.NotScanDirectories.Count);
+        }
+
+        private MyFileLauncher.ScanInfo CreateScanInfo()
+        {
+            // 設定ファイルフォーマットが変わった場合は他テストに影響出る前提、このテストでは共通の設定ファイルを使用する
+            return MyFileLauncher.ScanInfo.CreateInstance(@"TestData\UnitTestScanInfo\Scan.info");
+        }
+    }
+}
