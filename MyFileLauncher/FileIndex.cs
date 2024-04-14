04d634120221de41c3dc915109f5acecd5ab3f5d
FourHyphen <FourHyphen.4@gmail.com>
2024-04-15 08:07:57 +0900

    * indexファイルが存在するかを検知する処理を実装 * indexファイル作成処理を実装

---
+++ MyFileLauncher/FileIndex.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyFileLauncher
+{
+    internal class FileIndex
+    {
+        private static readonly string IndexFileName = "Index.info";
+
+        private string IndexFilePath { get; }
+
+        public static FileIndex CreateInstance()
+        {
+            string indexFilePath = System.IO.Path.GetFullPath(IndexFileName);
+            return CreateInstance(indexFilePath);
+        }
+
+        public static FileIndex CreateInstance(string indexFilePath)
+        {
+            return new FileIndex(indexFilePath);
+        }
+
+        private FileIndex(string indexFilePath)
+        {
+            IndexFilePath = indexFilePath;
+        }
+
+        /// <summary>
+        /// Index がすでに存在するかを返す
+        /// </summary>
+        public bool Exists()
+        {
+            return System.IO.File.Exists(IndexFilePath);
+        }
+
+        /// <summary>
+        /// インデックスファイルを作成する
+        /// </summary>
+        public void CreateIndexFile(ScanInfo scanInfo)
+        {
+            HashSet<string> all = GetFilePathes(scanInfo.ScanDirectories);
+            HashSet<string> files = ExcludeNotScanDirectoriesFromFileList(all, scanInfo.NotScanDirectories);
+            CreateIndexFile(files);
+        }
+
+        /// <summary>
+        /// 検索ディレクトリ内のサブディレクトリを含む全ファイル＆ディレクトリのフルパスを返す
+        /// </summary>
+        private HashSet<string> GetFilePathes(IReadOnlyCollection<string> searchDirs)
+        {
+            IEnumerable<string> pathes = searchDirs.SelectMany(dirPath => GetAllFilesIn(dirPath));
+            return pathes.ToHashSet();
+        }
+
+        /// <summary>
+        /// ディレクトリ内のサブディレクトリを含む全てのファイル＆ディレクトリ一覧を取得する
+        /// </summary>
+        private string[] GetAllFilesIn(string dirPath)
+        {
+            return System.IO.Directory.GetFileSystemEntries(dirPath, "*", System.IO.SearchOption.AllDirectories);
+        }
+
+        /// <summary>
+        /// ファイルリストから検索しないディレクトリのファイルを除外したリストを返す
+        /// </summary>
+        private HashSet<string> ExcludeNotScanDirectoriesFromFileList(IReadOnlyCollection<string> fileList, IReadOnlyCollection<string> notScanDirs)
+        {
+            // 検索しないディレクトリのファイルではないものを一覧化する
+            return fileList.Where(path => !DoStrStartWith(path, notScanDirs)).ToHashSet();
+        }
+
+        /// <summary>
+        /// 文字列がリスト内の文字列のいずれか 1 つ以上と前方一致するかを返す
+        /// </summary>
+        private bool DoStrStartWith(string str, IReadOnlyCollection<string> list)
+        {
+            return list.Where(s => str.StartsWith(s)).Count() >= 1;
+        }
+
+        /// <summary>
+        /// list を改行付き平坦化した文字列を中身としたインデックスファイルを作成する
+        /// </summary>
+        private void CreateIndexFile(HashSet<string> list)
+        {
+            string contents = string.Join("\r\n", list) + "\r\n";
+            System.IO.File.WriteAllText(IndexFilePath, contents);
+        }
+    }
+}
---
+++ TestMyFileLauncher/IntegrationTestFileIndex.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyFileLauncher;
+using System;
+using System.Linq;
+
+namespace TestMyFileLauncher
+{
+    [TestClass]
+    public class IntegrationTestFileIndex
+    {
+        private string BeforeEnvironment { get; set; } = Environment.CurrentDirectory;
+
+        [TestInitialize]
+        public void Init()
+        {
+            BeforeEnvironment = Environment.CurrentDirectory;
+            Environment.CurrentDirectory = Common.GetEnvironmentDirPath();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Environment.CurrentDirectory = BeforeEnvironment;
+        }
+
+        /// <summary>
+        /// Index ファイルが存在するかを検知する
+        /// </summary>
+        [TestMethod]
+        public void DetectWhetherExistsIndexFile()
+        {
+            string indexPath = @"TestData\IntegrationTestFileIndex\DetectWhetherExistsIndexFile\Index.info";
+
+            // 準備：すでに存在するなら削除
+            DeleteFileIfExists(indexPath);
+
+            // 存在しないことを検知
+            var fileIndex = MyFileLauncher.FileIndex.CreateInstance(indexPath);
+            Assert.IsFalse(fileIndex.Exists());
+
+            // 存在することを検知(中身は問わない)
+            CreateEmptyFile(indexPath);
+            Assert.IsTrue(fileIndex.Exists());
+
+            // 後始末
+            DeleteFileIfExists(indexPath);
+        }
+
+        private void CreateEmptyFile(string filePath)
+        {
+            // 空ファイル作成のため処理なし
+            using (System.IO.File.Create(filePath)) { }
+        }
+
+        private void DeleteFileIfExists(string filePath)
+        {
+            if (System.IO.File.Exists(filePath))
+            {
+                System.IO.File.Delete(filePath);
+            }
+
+            Assert.IsFalse(System.IO.File.Exists(filePath));
+        }
+
+        /// <summary>
+        /// 検索するディレクトリ内の全ファイルを含め、検索しないディレクトリ内のいずれのファイルも記載しないインデックスファイルを作成する
+        /// </summary>
+        [TestMethod]
+        public void CreateIndexFile()
+        {
+            string testWorkDirPath = @"TestData\IntegrationTestFileIndex\CreateIndexFile";
+            string scanInfoPath = $@"{testWorkDirPath}\Scan.info";
+            string indexPath = $@"{testWorkDirPath}\Index.info";
+            string notScanDirPath = $@"{testWorkDirPath}\not_scan";
+
+            // 準備：前回の残りがあるなら削除
+            CleanupTestCreateIndexFile(scanInfoPath, indexPath, notScanDirPath);
+
+            // 準備：テストディレクトリ設定と Scan.info ファイル作成
+            InitTestCreateIndexFile(scanInfoPath, indexPath, notScanDirPath);
+
+            // インデックスファイル作成
+            MyFileLauncher.ScanInfo si = MyFileLauncher.ScanInfo.CreateInstance(scanInfoPath);
+            MyFileLauncher.FileIndex fileIndex = MyFileLauncher.FileIndex.CreateInstance(indexPath);
+            fileIndex.CreateIndexFile(si);
+
+            // 結果確認：今回作成した Scan.info があり、検索しないディレクトリを含んでいなければ OK
+            string[] contents = System.IO.File.ReadAllLines(indexPath);
+            Assert.IsTrue(contents.Where(s => s.Contains(scanInfoPath)).Count() == 1);
+            Assert.IsFalse(contents.Where(s => s.Contains(notScanDirPath)).Any());
+
+            // 後始末
+            CleanupTestCreateIndexFile(scanInfoPath, indexPath, notScanDirPath);
+        }
+
+        private void CleanupTestCreateIndexFile(string scanInfoPath, string indexPath, string notScanDirPath)
+        {
+            DeleteFileIfExists(scanInfoPath);
+            DeleteFileIfExists(indexPath);
+            DeleteDirectoryIfExists(notScanDirPath);
+        }
+
+        private void DeleteDirectoryIfExists(string dirPath)
+        {
+            if (System.IO.Directory.Exists(dirPath))
+            {
+                System.IO.Directory.Delete(dirPath, recursive: true);
+            }
+
+            Assert.IsFalse(System.IO.Directory.Exists(dirPath));
+        }
+
+        private void InitTestCreateIndexFile(string scanInfoPath, string indexPath, string notScanDirPath)
+        {
+            // 検索しないディレクトリを作成
+            System.IO.Directory.CreateDirectory(notScanDirPath);
+            System.IO.File.WriteAllText($@"{notScanDirPath}\dummy.txt", "");
+
+            // Scan.info ファイル作成開始
+            using var stream = new System.IO.StreamWriter(scanInfoPath);
+
+            // 検索するディレクトリ：検索ディレクトリパスが環境依存にならないよう、Scan.info には Scan.info 保存先ディレクトリパスを記載
+            string? scanInfoDirFullPath = System.IO.Path.GetDirectoryName(Common.GetFilePathOfDependentEnvironment(scanInfoPath));
+            stream.WriteLine(scanInfoDirFullPath);
+
+            // 検索しないディレクトリ：先頭に "-" を付与
+            stream.WriteLine($@"-{Common.GetFilePathOfDependentEnvironment(notScanDirPath)}");
+        }
+    }
+}
