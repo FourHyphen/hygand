e30cde6db98974b289a783b1e7f1cb4f5b096ab4
FourHyphen <FourHyphen.4@gmail.com>
2024-05-04 12:04:17 +0900

    履歴をファイルから読み込む処理を実装

---
+++ MyFileLauncher/History.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFileLauncher
+{
+    internal class History
+    {
+        private static readonly string HistoryFileName = "History.info";
+
+        internal IReadOnlyCollection<string> Files { get; }
+
+        internal static History CreateInstance()
+        {
+            string historyFilePath = System.IO.Path.GetFullPath(HistoryFileName);
+            return CreateInstance(historyFilePath);
+        }
+
+        internal static History CreateInstance(string historyFilePath)
+        {
+            return new History(historyFilePath);
+        }
+
+        private History(string historyFilePath)
+        {
+            Files = System.IO.File.ReadAllLines(historyFilePath);
+        }
+    }
+}
---
+++ TestMyFileLauncher/UnitTestHistory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMyFileLauncher
+{
+    [TestClass]
+    public class UnitTestHistory
+    {
+        private string BeforeEnvironment { get; set; } = Environment.CurrentDirectory;
+
+        [TestInitialize]
+        public void Init()
+        {
+            BeforeEnvironment = Environment.CurrentDirectory;
+            Environment.CurrentDirectory = Common.GetEnvironmentDirPath();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Environment.CurrentDirectory = BeforeEnvironment;
+        }
+
+        /// <summary>
+        /// 履歴一覧をファイルから取得する
+        /// </summary>
+        [TestMethod]
+        public void GetHistoryFromHistoryInfoFile()
+        {
+            MyFileLauncher.History history = CreateHistory();
+
+            Assert.IsTrue(history.Files.Contains(@"C:\with space"));
+            Assert.IsTrue(history.Files.Contains(@"D:\日本語\"));
+
+            Assert.AreEqual(expected: 2, actual: history.Files.Count);
+        }
+
+        private MyFileLauncher.History CreateHistory()
+        {
+            // 設定ファイルフォーマットが変わった場合は他テストに影響出る前提、このテストでは共通の設定ファイルを使用する
+            return MyFileLauncher.History.CreateInstance(@"TestData\UnitTestHistory\History.info");
+        }
+    }
+}
